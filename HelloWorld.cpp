df170bb651578ac9c280fa4d3a82c8ef4451b63d
Blinks-Scripts <85519449+Blinks-Scripts@users.noreply.github.com>
2022-11-01 21:04:47 -0500

    .exe Compile update
    Checking if .exe works on Windows with different compiler options (2)
    
    using -static -static-libgcc and -static-libstdc++
    
    Needed to add a std::cin prompt to keep window from closing immediately

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -12,7 +12,11 @@ int main() {
     // Print "Hello, World!" (minus the quotation marks)
     std::cout << "Hello, World!" << std::endl;
 
-    return 0; // Return 0 for successful execution
+    char c;
+    if ( std::cin >> c ) // require input to close window
+        return 0; // Return 0 for successful execution
+
+    return 1; // We should not get here
 }
 
 #endif // HELLOWORLD_CPP
