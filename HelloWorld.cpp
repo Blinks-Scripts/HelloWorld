c4c9e2c8ca3bed0325e2135bf4e31373ce63992b
Blinks-Scripts <85519449+Blinks-Scripts@users.noreply.github.com>
2022-11-01 20:32:31 -0500

    Added initial program
    Program is complete.
    
    It prints "Hello, World!" to the screen. It's a very cheerful program!

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+#ifndef HELLOWORLD_CPP
+#define HELLOWORLD_CPP
+
+// Jeffrey Blinks
+//
+// A simple "Hello World" program
+
+#include <iostream>
+
+int main() {
+
+    // Print "Hello, World!" (minus the quotation marks)
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0; // Return 0 for successful execution
+}
+
+#endif // HELLOWORLD_CPP
